9073fbc9561836bafa432a0fb7216ab700352cc3
Ormary Montilla <ormary.montilla@gmail.com>
2022-05-16 18:19:44 -0300

    Agregando menú e-commerce

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css';
+import Logo from './Img/Logo.png'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h1> 
+      <img src={Logo} className='logo'></img>
+      </h1>
+      <div className='menu-ecommerce'>
+        <button>Home</button>
+        <button>Productos</button>
+        <button>Contacto</button>
+        <button>Blog</button>
+
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
